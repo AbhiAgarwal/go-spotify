a3219fc4a88f5a3ef7a5b88de64fda20a5533930
Abhi Agarwal <me@abhiagarwal.com>
2014-11-12 12:51:57 -0500

    Adding HTTP example

--- examples/http.go
+++ examples/http.go
@@ -6,9 +6,46 @@ import (
 )
 
 func main() {
+	// Gets command and initializes martini
+	commands := spotify.Commands()
 	m := martini.Classic()
+
+	// Gets the list of commands to what they do, and appends them
+	currentString := ""
+	for k := range commands {
+		currentString += k + " - " + commands[k] + "\n"
+	}
+
+	// Runs the martini server with our given string
 	m.Get("/", func() string {
-		return "Hello world!"
+		return currentString
 	})
+
+	m.Get("/:name", func(params martini.Params) string {
+		if val, ok := commands[params["name"]]; ok {
+			// Handle Edge Cases
+			if params["name"] == "playPlaylist" || params["name"] == "playTrack" {
+				return "Needs a second parameter!"
+			}
+			// Generate Case will just work
+			spotify.Execute(val)
+			return "Command has been executed"
+		}
+		return "Command does not exist"
+	})
+
+	m.Get("/:name/:second", func(params martini.Params) string {
+		if val, ok := commands[params["name"]]; ok {
+			// Handle Edge Cases
+			if params["name"] != "playPlaylist" && params["name"] != "playTrack" {
+				return "Can't have a second parameter!"
+			}
+			// Generate Case will just work
+			spotify.Execute(spotify.Format(val, params["second"]))
+			return "Command has been executed"
+		}
+		return "Command does not exist"
+	})
+
 	m.Run()
 }
